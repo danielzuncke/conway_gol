40dcf772f87ead34f62b1d105773d6609043a6fb
Daniel Zuncke <daniel.zuncke@gmail.com>
2019-05-29 12:10:39 +0200

    edited docstrings

--- game_of_life.py
+++ game_of_life.py
@@ -4,29 +4,29 @@ import os
 from time import sleep
 
 class GameOfLife:
-    """ Recreates Conway's game of life and can be printed to commandline """
+    """Recreates Conway's game of life and can be printed to commandline
 
-    def __init__(self, length, bordertype, fps=500, generations=1):
-        """
-        length       length of dimensions\n
-        bordertype   define bordertype: void, wrap or mirror\n
-        fps          redraw-rate in ms\n
-        generations  number of generations simulated
-        """
+    Args:
+        length:      length of dimensions
+        bordertype:  define bordertype: void, wrap or mirror
+    """
+
+    def __init__(self, length, bordertype):
         self.l = length
         self.A = np.random.randint(2, size=(self.l, self.l))
         self.b = bordertype
-        self.fps = fps
-        self.generations = generations
         ...
 
     def iterate(self):
         temp = np.zeros((self.l, self.l))
         ...
-    ...
 
     def arenaString(self):
-        """ draws playing field for each generation """
+        """
+        Returns:
+            string representing the playing field with dead (0)
+            and alive (1) cells
+        """
         output = '\n'
         for i in range(self.l):
             for j in range(self.l):
@@ -34,13 +34,21 @@ class GameOfLife:
             output += '\n'
         return output
 
-    def draw(self):
-        for i in range(self.generations):
+    def draw(self, generations=1, t=500):
+        """
+        Prints playing field
+
+        Args:
+            generations: how many evolutions will be simulated
+            t:           redraw frequenzy in ms
+        """
+        for i in range(generations):
             os.system('cls')  # on windows
             # os.system('clear')  #on linux / os x
             print(self.arenaString())
             self.iterate()
-            sleep(.5)
+            if i != (generations - 1):
+                sleep(t / 1000)
         ...
 
 
