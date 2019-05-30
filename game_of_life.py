3e2ed5ecc71432e8c0b62809f02f67534919b1d3
Daniel Zuncke <daniel.zuncke@gmail.com>
2019-05-30 02:46:00 +0200

    cooked some spaghetti, added scaleability to output pngs

--- game_of_life.py
+++ game_of_life.py
@@ -3,7 +3,6 @@ import cursor
 import os
 import sys
 import cv2
-from time import sleep
 
 class GameOfLife:
     """
@@ -19,83 +18,63 @@ class GameOfLife:
     """
 
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.Arena = np.random.randint(2, size=(self.height, self.width))
+        self.Arena = np.random.randint(2, size=(height, width))
         self.progress = []  # stores only last 4 generations
         self.progress.append(self.Arena)
 
-    def iterate(self):
+    def iterate(self, A):
         """
         Creates next generation and append to progress list
         """
-        temp = np.zeros((self.height, self.width))
+        temp = np.zeros((A.shape[0], A.shape[1]))
         # cell who's neighbors are counted A[x, y]
-        for x in range(self.height):
-            for y in range(self.width):
+        for x in range(A.shape[0]):
+            for y in range(A.shape[1]):
                 # counting neighbors and spawning, killing cells in accordance
                 neighbors = 0
                 for i in range(3):
-                    if (x - 1 + i) < 0 or (x - 1 + i) == self.height:
+                    if (x - 1 + i) < 0 or (x - 1 + i) == A.shape[0]:
                         continue
                     for j in range(3):
-                        if (y - 1 + j) < 0 or (y - 1 + j) == self.width:
+                        if (y - 1 + j) < 0 or (y - 1 + j) == A.shape[1]:
                             continue
                         if j == 1 and i == 1:
                             continue
-                        if self.progress[-1][x - 1 + i, y - 1 + j] == 1:
+                        if A[x - 1 + i, y - 1 + j] == 1:
                             neighbors += 1
                 if neighbors == 3:
                     temp[x, y] = 1
-                elif neighbors == 2 and self.progress[-1][x, y] == 1:
+                elif neighbors == 2 and A[x, y] == 1:
                     temp[x, y] = 1
         self.progress.append(temp)
+        # TODO: break if caught in recurring loop
 
-        # break loop if it doesnt change anymore
-        # partly implement: checks last element vs 2 and 3 generations before
-        if len(self.progress) == 5:
-            if (np.all(self.progress[-1] == self.progress[-3]) or
-                    np.all(self.progress[-1] == self.progress[-4])):
-                cursor.show()
-                sys.exit('stuck in recurrent loop')
-            self.progress.pop(0)
-
-    def toString(self, x=-1):
-        """
-        Returns matrix formatted for console output
-
-        Args:
-            x:  generation to return; standard: last generation, 4 are stored
-
-        Returns:
-            string representing the playing field with dead and alive cells
-        """
-        output = '\n'
-        for i in range(self.height):
-            for j in range(self.width):
-                if self.progress[x][i, j] == 0:
-                    output += ' '
-                else:
-                    output += '█'  # probable bests: █, ▄, ▓, ░, ■
-            output += '\n'
-        return output
-
-    def toPNG(self, i):
+    def toPNG(self, A, scale, x=0):
         """
         Creates ordered PNGs to animate the game of life
         """
-        cv2.imwrite('output_' + str(i) + '.png', self.progress[-1] * 255)  # pylint: disable=E1101
-        print(f'generation: {i}')
+        for i in range(scale):
+            A = self.doubleTime(A)
+        cv2.imwrite('output_' + str(x) + '.png', A * 255)  # pylint: disable=E1101
+        print(f'gen: {x}')
 
-    def toCMD(self, i):
+    def toCMD(self, A, x=0):
         """
         Prints playing field to CMD
         """
         os.system('cls')  # on windows
-        print(self.toString())
-        print('gen:', i)
+        output = '\n'
+        for i in range(A.shape[0]):
+            for j in range(A.shape[1]):
+                if A[i, j] == 0:
+                    output += ' '
+                else:
+                    output += '█'  # probable bests: █, ▄, ▓, ░, ■
+            output += '\n'
+        print(output)
+        print(f'gen: {x}')
 
-    def loop(self, generations=1, toCMD=None, toPNG=None):
+    def loop(self, generations=1, toCMD=None, singlePNG=None, singlePNGscale=0, multiPNG=None, multiPNGscale=0):
         """
         Plays the game of life and prints either to CMD or to PNGs
 
@@ -105,15 +84,33 @@ class GameOfLife:
             toCMD:        if True the game will be saved as PNGs
         """
         cursor.hide()
-        for i in range(generations):
+        for i in range(generations - 1):
             if toCMD:
-                self.toCMD(i)
-            if toPNG:
-                self.toPNG(i)
-            self.iterate()
+                self.toCMD(self.progress[-1], i)
+            if multiPNG:
+                self.toPNG(self.progress[-1], multiPNGscale, i)
+            self.iterate(self.progress[-1])
+        if toCMD:
+            self.toCMD(self.progress[-1], generations)
+        if multiPNG:
+            self.toPNG(self.progress[-1], multiPNGscale, generations)
+        if singlePNG:
+            self.toPNG(self.progress[-1], singlePNGscale, generations)
         cursor.show()
+
+    def doubleTime(self, A):
+        temp = np.zeros(dtype=int, shape=(
+            2 * A.shape[0], 2 * A.shape[1]))
+        for i in range(A.shape[0]):
+            for j in range(A.shape[1]):
+                temp[2 * i, 2 * j] = A[i, j]
+                temp[2 * i + 1, 2 * j] = A[i, j]
+                temp[2 * i, 2 * j + 1] = A[i, j]
+                temp[2 * i + 1, 2 * j + 1] = A[i, j]
+        return temp
 
 
 if __name__ == "__main__":
     test = GameOfLife(int(input('width: ')), int(input('height: ')))
-    test.loop(generations=(int(input('generations: '))) + 1, toPNG=True)
+    test.loop(generations=(int(input('generations: '))),
+              toCMD=True, multiPNG=True, multiPNGscale=3)
