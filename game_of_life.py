391977973bcd97709e00093a8a0ff21cbcdb44a1
Daniel Zuncke <daniel.zuncke@gmail.com>
2019-06-03 02:27:02 +0200

    trying to implement multithreading for def iterate(), encountered bugs: png prints initiated matrix, then for each matrix in the list the last one; algorithm to calculate next matrix is wrong; self.progress list gets changed to contain only the first matrix and the last matrix

--- game_of_life.py
+++ game_of_life.py
@@ -39,10 +39,39 @@ class GameOfLife:
         self.width = width
         self.height = height
         self.statusString = '\n\n'
-        self.progress = [np.random.randint(2, size=(height, width),
-                                           dtype=np.int)]
+        self.progress = [np.ones((5, 5), dtype=np.int)]
+        for i in range(5):
+            for j in range(5):
+                if i == 1 or i == 3:
+                    self.progress[0][i, j] = 0
+        print(f'initiated Matrix:\n{self.progress[0]}\n')
+        # self.progress = [np.random.randint(2, size=(height, width),
+        #                                   dtype=np.int)]
+        self.temp = np.zeros((self.height, self.width), dtype=np.int)
 
     # TODO: implement faster algorithm (that can make use of multithreading)
+
+    def countNeighbors(self, A, x, y):
+        neighbors = 0
+        for i in range(3):
+            if (x - 1 + i) < 0 or (x - 1 + i) == self.height:
+                continue
+            for j in range(3):
+                if (y - 1 + j) < 0 or (y - 1 + j) == self.width:
+                    continue
+                if j == 1 and i == 1:
+                    continue
+                if A[x - 1 + i, y - 1 + j] == 1:
+                    neighbors += 1
+        if neighbors == 3:
+            self.temp[x, y] = 1
+            return
+        elif neighbors == 2 and A[x, y] == 1:
+            self.temp[x, y] = 1
+            return
+        self.temp[x, y] = 0
+        return
+
     def iterate(self, A, status=False):
         """
         Creates next generation and appends it to progress list
@@ -52,27 +81,40 @@ class GameOfLife:
         """
         if status:
             t1 = milli()
-        temp = np.zeros((A.shape[0], A.shape[1]))
+        threads = []
+        for x in range(self.height):
+            for y in range(self.width):
+                # t = threading.Thread(target=(lambda x, y: temp[x, y]=self.countNeighbors(x, y))), args = (x, y,))
+                t = threading.Thread(
+                    target=(lambda A, x, y: self.countNeighbors(A, x, y)), args=(A, x, y,))
+                threads.append(t)
+                t.start()
+                t.join()
+        # [t.start() for t in threads]
+        # [t.join() for t in threads]
+        print(f'temp:\n{self.temp}\n')
+        self.progress.append(self.temp)
+        print(f'temp added:\n{self.progress[-1]}\n')
         # cell who's neighbors are counted A[x, y]
-        for x in range(A.shape[0]):
-            for y in range(A.shape[1]):
-                # counting neighbors and spawning, killing cells in accordance
-                neighbors = 0
-                for i in range(3):
-                    if (x - 1 + i) < 0 or (x - 1 + i) == A.shape[0]:
-                        continue
-                    for j in range(3):
-                        if (y - 1 + j) < 0 or (y - 1 + j) == A.shape[1]:
-                            continue
-                        if j == 1 and i == 1:
-                            continue
-                        if A[x - 1 + i, y - 1 + j] == 1:
-                            neighbors += 1
-                if neighbors == 3:
-                    temp[x, y] = 1
-                elif neighbors == 2 and A[x, y] == 1:
-                    temp[x, y] = 1
-        self.progress.append(temp)
+        # for x in range(A.shape[0]):
+        #    for y in range(A.shape[1]):
+        #        # counting neighbors and spawning, killing cells in accordance
+        #        neighbors = 0
+        #        for i in range(3):
+        #            if (x - 1 + i) < 0 or (x - 1 + i) == A.shape[0]:
+        #                continue
+        #            for j in range(3):
+        #                if (y - 1 + j) < 0 or (y - 1 + j) == A.shape[1]:
+        #                    continue
+        #                if j == 1 and i == 1:
+        #                    continue
+        #                if A[x - 1 + i, y - 1 + j] == 1:
+        #                    neighbors += 1
+        #        if neighbors == 3:
+        #            temp[x, y] = 1
+        #        elif neighbors == 2 and A[x, y] == 1:
+        #            temp[x, y] = 1
+
         if status:
             self.statusString += ('def iterate in ms: ' +
                                   str(milli() - t1) + '\n')
@@ -126,13 +168,16 @@ class GameOfLife:
         """
         threads = []
         for x, A in enumerate(self.progress):
-            t = threading.Thread(target=(lambda x: cv2.imwrite(  # pylint: disable=E1101
-                'output_' + str(x) + '.png',
-                np.kron(A, np.ones((scale, scale),
-                                   dtype=np.int) * 255))), args=(x,))
-            threads.append(t)
-        [t.start() for t in threads]
-        [t.join() for t in threads]
+            # t = threading.Thread(target=(lambda x: cv2.imwrite(  # pylint: disable=E1101
+            #    'output_' + str(x) + '.png',
+            #    np.kron(A, np.ones((scale, scale),
+            #                       dtype=np.int) * 255))), args=(x,))
+            # threads.append(t)
+            cv2.imwrite('output_' + str(x) + '.png', np.kron(A,
+                                                             np.ones((scale, scale), dtype=np.int) * 255))
+            print(f'list progress {x}:\n{A}\n')
+        #[t.start() for t in threads]
+        #[t.join() for t in threads]
 
     def toCMD(self, A, x=0):
         """
@@ -181,12 +226,12 @@ class GameOfLife:
                 self.statusString += 'generation: ' + str(i) + '\n'
             if toCMD:
                 self.toCMD(self.progress[-1], i)
-            if self.caught(depth, status):
-                if status:
-                    print(f'{self.statusString}\n')
-                    self.statusString = ''
-                print('caught in loop')
-                break
+            # if self.caught(depth, status):
+            #    if status:
+            #        print(f'{self.statusString}\n')
+            #        self.statusString = ''
+            #    print('caught in loop')
+            #    break
             if i != generations:
                 self.iterate(self.progress[-1], status)
             if status:
