0d9b0c5bf2e1f6647a43b319efdd055a45a7b87d
Daniel Zuncke <daniel.zuncke@tum.de>
2019-11-13 19:59:41 +0100

    fixing branching

--- README.md
+++ README.md
@@ -1,32 +1,15 @@
 # Conway's game of life
-built in python 3.7.3\
-\
-**version on master branch might not be in working condition, select latest working_state branch instead**
 
-## packages
+built in python 3.7.3
+
+## Packages
 
 * cursor
 * numpy
 * opencv-python
 
 ## TODO's
-
-* def iterate: implement multiprocessing
-* clean up code from tests
-* multithread cmd outputs
-* update documentation
-* pause on keyhit, enter menu
-* menu with ability to:
-  * save and load matrix
-  * import matrix from png
-  * continue simulation (for n iterations)
-
-## rules for the game
-
-* playing field is initiated at random with dead and alive cells
-* a dead cell gets revived when it has exactly three living neighbors
-* a living cell with two or three living neighbors stays alive, otherwise it dies\
-[Wikipedia](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life "Wikipedia: Conway's game of life")
+(none in working examples)
 
 ## Example gif from PNGs
 ![](gol.gif)
--- game_of_life.py
+++ game_of_life.py
@@ -2,76 +2,56 @@ import numpy as np
 import cursor
 import os
 import shutil
-import sys
 import cv2
-import time
-from math import ceil
-from concurrent.futures import ProcessPoolExecutor
-# TODO: multithreading for cmd output
-# from concurrent.futures import ThreadPoolExecutor
 
-def milli(): return int(round(time.time() * 1000))
 
 class GameOfLife:
     """
-    Recreates Conway's game of life and can be printed to commandline
+    Recreates Conway's game of life and can be printed to cmd or to PNG series
 
     Args:
-        width:     width of matrix
-        height:    height of matrix
+        width:    width of matrix
+        height:   height of matrix
 
     Vars:
         width:    width of matrix
         height:   height of matrix
-        scaleMatrix:  initiated as None, when the matrix needs to be scaled
-                      to make image it will be sized to be the multiplier in
-                      the matrix multiplication
-        Arena:    randomly generated first generation
+        src_path: script path
+        dst_path: path to which generated files are moved
         progress: list containing all generations
 
     Functions:
-        iterate:   calculates next generation
-        caught:    returns True if game is caught in loop
-        setScaleMatrix:  can be called to set scaleMatrix
-        scaleUp:   scales a given matrix up for better presentation
-        toPNG:     saves given matrix to png file
-        toCMD:     prints a given matrix to command line
-        loop:      plays the game
+        countNeighbors:  returns how many living cells surround target cell
+        iterate:  calculates next generation
+        caught: checks if game is caught in a loop
+        toPNG:  saves given matrix or list of matrices to png file
+                pixel size is scalable
+        toCMD:  prints a given matrix to command line
+        loop:   plays the game
     """
 
-    def __init__(self, width, height, max_processes=1):
+    def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.max_processes = max_processes
-        self.src_path = os.path.abspath('.') + ('\\\\')
-        self.dst_path = os.path.abspath('.') + ('\\\\output\\\\')
+        self.src_path = os.path.abspath('.') + ('\\')
+        self.dst_path = os.path.abspath('.') + ('\\output\\')
         self.progress = [np.random.randint(2, size=(height, width),
                                            dtype=np.int)]
         self.temp = np.zeros((self.height, self.width), dtype=np.int)
 
-    # TODO: global processHandler probably better, otherwise it needs to get
-    # initiated for every step
-    # processHandler should only call functions and not include the functional
-    # details within
-    def processHandler(self, iterate=False, multiPNG=False, singlePNG=False):
-        with ProcessPoolExecutor(max_workers=self.max_processes) as executor:
-            if iterate:
-                submatrices = [None for x in range(self.max_processes)]
-                for x in range(self.max_processes):
-                    if self.height > self.width:
-                        submatrices[x] = executor.submit(self.iterate(self.progress[-1][x * ceil(
-                            len(self.height)) / self.max_processes:(x + 1) * ceil(len(self.height)) / self.max_processes]))
-                        ...
-                    else:
-                        ...
-                ...
-            elif multiPNG:
-                ...
-            elif singlePNG:
-                ...
-        return
-
     def countNeighbors(self, A, x, y):
+        """
+        Counts alive neighbors of a given cell in matrix and returns next gen
+        cell status
+
+        Args:
+            A:    matrix that contains the cell of interest
+            x:    position of cell
+            y:    position of cell
+
+        Returns:
+            None
+        """
         neighbors = 0
         for i in range(3):
             if (x - 1 + i) < 0 or (x - 1 + i) == self.height:
@@ -92,21 +72,18 @@ class GameOfLife:
         self.temp[x, y] = 0
         return
 
-    # TODO: implement multiprocessing additionally (divide matrix in similar
-    #       pieces and calculate them independently)
-    # TODO: search for parts that don't change
-    def iterate(self, A, worker):
+    def iterate(self, A):
         """
-        Creates next generation and appends it to progress list
+        Calculates next generation and appends it to progress list
 
         Args:
             A:    matrix to evaluate
         """
         for x in range(self.height):
             for y in range(self.width):
+                self.countNeighbors(A, x, y)
         self.progress.append(self.temp.copy())
 
-    # TODO: implement optimise for multiprocessing
     def caught(self, depth=2):
         """
         Checks if game is stuck by looking for duplicates in progress list
@@ -135,28 +112,28 @@ class GameOfLife:
                 return True
         return False
 
-    def toPNG(self, scale, singlePNG=None):
+    def toPNG(self, scale=5, singlePNG=None):
         """
-        Creates ordered PNGs
+        Creates ordered PNGs; cell-size adjustable, standard 5 by 5 pixels
 
         Args:
-            A:      matrix that will be printed to png
-            x:      current generation (to view progress in command line)
-            scale:  scales the size that one matrix value will
-                    take in pixels
+            scale:    scales the size that one matrix value will
+                      take in pixels
+            singlePNG:  only saves last Matrix in list
         """
         if singlePNG is None:
-            with ProcessPoolExecutor(max_workers=self.max_processes) as executor:
-                for x, A in enumerate(self.progress):
-                    executor.submit(cv2.imwrite('output_' + str(x) + '.png',  # pylint: disable=E1101
-                                                np.kron(A, np.ones((scale, scale),
-                                                                   dtype=np.int) * 255)))
-                    shutil.move(self.src_path + 'output_' + str(x) + '.png',
-                                self.dst_path + 'output_' + str(x) + '.png')
+            for x, A in enumerate(self.progress):
+                cv2.imwrite(  # pylint: disable=E1101
+                    'output_' + str(x) + '.png',
+                    np.kron(A, np.ones((scale, scale), dtype=np.int) * 255))
+                shutil.move(self.src_path + 'output_' + str(x) + '.png',
+                            self.dst_path + 'output_' + str(x) + '.png')
         else:
-            cv2.imwrite('single_output.png', np.kron(A,  # pylint: disable=E1101
-                                                     np.ones((scale, scale), dtype=np.int) * 255))
-            shutil.move(self.src_path + 'single_output.png', self.dst_path + 'single_output.png')
+            cv2.imwrite('single_output.png',  # pylint: disable=E1101
+                        np.kron(self.progress[-1],
+                                np.ones((scale, scale), dtype=np.int) * 255))
+            shutil.move(self.src_path + 'single_output.png',
+                        self.dst_path + 'single_output.png')
 
     def toCMD(self, A, x=0):
         """
@@ -173,7 +150,7 @@ class GameOfLife:
                 if A[i, j] == 0:
                     output += ' '
                 else:
-                    output += '█'  # probable bests: █, ▄, ▓, ░, ■
+                    output += '█'  # good chars: █, ▄, ▓, ░, ■
             output += '\n'
         print(output)
         print(f'gen: {x}')
@@ -181,45 +158,41 @@ class GameOfLife:
     def loop(self, generations=1, toCMD=None, singlePNG=None, singlePNGscale=1,
              multiPNG=None, multiPNGscale=1, loopLen=5, depth=2):
         """
-        Plays the game of life and prints either to CMD or to PNGs
+        Plays the game of life, can print to cmd or save as png's
 
         Args:
-            generations:     number of iterations
-            toCMD:           if True the game will be printed to the
-                             command line
-            singlePNG:       if True the final outcome will be saved as PNG
+            generations:  number of iterations
+            toCMD:    if True the game will be printed to the
+                      command line
+            singlePNG:  if True the final outcome will be saved as PNG
             singlePNGscale:  scales the size that one matrix value will
                              take in pixels
-            multiPNG:        if True the playthrough will be saved as PNG
-            multiPNGscale:   scales the size that one matrix value will
-                             take in pixels
-            loopLen:         number of matrizes that are recorded going back
-                             from most recent
+            multiPNG: if True the playthrough will be saved as PNG
+            multiPNGscale:  scales the size that one matrix value will
+                            take in pixels
+            loopLen:  number of matrizes that are recorded going back
+                      from most recent
+            depth:    how deep list checks for duplicates
         """
         if toCMD:
             cursor.hide()
         for i in range(generations + 1):
-            t = milli()
             if toCMD:
                 self.toCMD(self.progress[-1], i)
             if self.caught(depth):
                 print('caught in loop')
                 break
             if i != generations:
-                self.processHandler(iterate=True)
-                # self.iterate(self.progress[-1])
-            print(f'finished {i} in {(milli() - t)/1000}s')
+                self.iterate(self.progress[-1])
         if multiPNG:
-            self.processHandler(multiPNG=True)
-            # self.toPNG(multiPNGscale)
+            self.toPNG(multiPNGscale)
         if singlePNG:
-            self.processHandler(singlePNG=True)
-            # self.toPNG(singlePNGscale, self.progress[-1])
+            self.toPNG(singlePNGscale, self.progress[-1])
         if toCMD:
             cursor.show()
 
 
 if __name__ == "__main__":
-    test = GameOfLife(int(input('width: ')), int(input('height: ')))
-    test.loop(generations=(int(input('generations: '))),
-              toCMD=False, multiPNG=True, multiPNGscale=5)
+    example = GameOfLife(int(input('width: ')), int(input('height: ')))
+    example.loop(generations=(int(input('generations: '))),
+                 multiPNG=True, multiPNGscale=5)
